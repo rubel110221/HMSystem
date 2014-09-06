df3d87fde93d4ad04d88fd0334d2489979d8363a
Muhitur Rahman <abm.cse.ku@gmail.com>
2014-09-07 02:56:29 +0600

    Muhitur110204 24th commit

---
+++ HospitalMS/view/mainmenu_patient_facilities.php
@@ -0,0 +1,42 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle14">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Patient Facilities</h2>
+						
+						<ul>
+						<li><a href="">Accommodations</a></li>
+						<li><a href="">Bill of rights</a></li>
+						<li><a href="">Cafeteria</a></li>
+						<li><a href="">Facilities</a></li>
+						<li><a href="">Facilities</a></li>
+						<li><a href="">Meal Provision</a></li>
+						<li><a href="">Others</a></li>	
+						<li><a href="">Pharmacy</a></li>	
+						<li><a href="">Playground</a></li>
+						<li><a href="">Roof Top Garden</a></li>
+						
+			
+						</ul>
+					
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
---
+++ HospitalMS/view/mainmenu_service_support_facilities.php
@@ -0,0 +1,38 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle15">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Service Support</h2>
+					    
+   <p> Professional <b>USA-standard</b> housekeeping & cleaning services</p>
+    <p>24-hours pharmacy services</p>
+    <p>Convenient & secured car parking garage</p>
+    <p>Full-service cafeteria & snacks cafe</p>
+    <p>Computer info access</p>
+    <p>USA-standard safety & security system</p>
+    <p>Prayer rooms</p>
+    <p>Ground Ambulance Service: A fleet of total 6 ambulances, which includes state of the art Mercedes Benz Ambulances with all cardiac life support and ICU facilities
+    <p>Square Air Ambulance Helicopter (a brand new Bell 407, USA): It has been specially adapted for its purpose, to enable patients to be treated to the standard of an intensive care unit in flight
+    <p>The only fully functional Helipad on the roof of Square Hospital and an exclusive entrance to Emergency Department
+
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
