85e82a022ca08c2c3a5e7ec1cd1edb996ba72081
Muhitur Rahman <abm.cse.ku@gmail.com>
2014-09-07 02:52:51 +0600

    Muhitur110204 22th Commit

---
+++ HospitalMS/view/mainmenu_about_us.php
@@ -0,0 +1,35 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle10">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>About Us</h2>
+						
+						<ul>
+						<li><a href="">A Few Words</a></li>
+						<li><a href="">About Square Group</a></li>
+						<li><a href="">About Square Hospital</a></li>
+						<li><a href="">Contact</a></li>
+						<li><a href="">Mission</a></li>
+						<li><a href="">Vision</a></li>			
+						</ul>
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
---
+++ HospitalMS/view/mainmenu_clinics_and_centres.php
@@ -0,0 +1,26 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle12">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Clinics and Centres</h2>
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
