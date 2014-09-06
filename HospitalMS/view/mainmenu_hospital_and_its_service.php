9770544616178ed69929a7776449c27c03db0a6d
Muhitur Rahman <abm.cse.ku@gmail.com>
2014-09-07 02:55:09 +0600

    Muhitur 23th commit

---
+++ HospitalMS/view/mainmenu_hospital_and_its_service.php
@@ -0,0 +1,36 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle11">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Hospital And Its Services</h2>
+						<ul>
+						<li><a href="">A Few Words</a></li>
+						<li><a href="">Ancillary Services</a></li>
+						<li><a href="">Executive Health Check-up</a></li>
+						<li><a href="">Inpatient Services</a></li>
+						<li><a href="">Medical Specialties</a></li>
+						<li><a href="">Outpatient Services</a></li>			
+						<li><a href="">Service Support</a></li>	
+						
+						</ul>
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
---
+++ HospitalMS/view/mainmenu_partners.php
@@ -0,0 +1,32 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+				<div id="sidebar-middle13">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Our Partners</h2>
+						
+						<ul>
+						<li><a href="">Affiliated Partner</a></li>
+						<li><a href="">Corporate Partners</a></li>								
+						</ul>
+					
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
