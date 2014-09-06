6088a0f5355cf9b62f21bf7d07110fab63c5bc02
Muhitur Rahman <abm.cse.ku@gmail.com>
2014-09-07 02:58:37 +0600

    Muhitur110204 25th commit

---
+++ HospitalMS/view/mainmenu_useful_links.php
@@ -0,0 +1,36 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+			<div id="sidebar-middle17">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Quick Links</h2>
+						
+						<ul>
+						<li><a href="">Emergency Numbers</a></li>
+						<li><a href="">Find a doctor</a></li>
+						<li><a href="">Ask a Question</a></li>
+						<li><a href="">Make an Appointment</a></li>
+						<li><a href="">Health Checkup</a></li>
+						<li><a href="">Brochures</a></li>			
+						</ul>
+					
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
---
+++ HospitalMS/view/mainmenu_visitors_facilities.php
@@ -0,0 +1,36 @@
+<div class="sidebar">
+				 
+				<?php
+					require_once('view/sidebar_left.php');
+				?>
+			<div id="sidebar-middle16">
+					<div class="breadcrumb">
+						<a href="index.html">Home</a>
+					</div>
+					<div id="description">
+						<h2>Visitors Facilities</h2>
+						
+						<ul>
+						<li><a href="">Cafeteria</a></li>
+						<li><a href="">Car parking</a></li>
+						<li><a href="">Instructions for Visitors</a></li>
+						<li><a href="">OPD Pharmacy</a></li>
+						<li><a href="">Prayer Room</a></li>
+						<li><a href="">Visiting Hour</a></li>			
+						</ul>
+					
+					</div>
+				</div>
+							  
+				
+				<?php
+					require_once('view/sidebar_right.php');
+				?>			 
+				
+</div>	 
+			<div id="footer">
+			<?php
+					require_once('view/footer_menu.php');
+			?>
+			</div>
+			</div>
